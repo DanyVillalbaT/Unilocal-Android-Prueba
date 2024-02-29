3b291fd245f6a82c515b3360bfbcdaa27cce6c09
DanyVillalbaT <villalba.daniela.0874@eam.edu.co>
2024-02-28 19:01:09 -0500

    Creados 3 botones con su funcionalidad para las actividades

---
+++ app/src/main/java/co/edu/eam/unilocal_prueba/CrearLugarActivity.kt
@@ -0,0 +1,11 @@
+package co.edu.eam.unilocal_prueba
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CrearLugarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crear_lugar)
+    }
+}
--- app/src/main/java/co/edu/eam/unilocal_prueba/MainActivity.kt
+++ app/src/main/java/co/edu/eam/unilocal_prueba/MainActivity.kt
@@ -1,11 +1,36 @@
 package co.edu.eam.unilocal_prueba
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun irARegistro(v: View){
+        //Imprimir mensaje en consola, hay diferentes tipos, de error, warning , etc
+        Log.e("MainActivity", "Se escucha el evento del click")
+        val intentRegistro = Intent(this, RegistroActivity::class.java)
+        startActivity(intentRegistro)
+    }
+
+    fun irALogin(v: View){
+        //Imprimir mensaje en pantalla, un mensaje flotante corto o largo
+        Toast.makeText(this, "Se dió click al botón del login", Toast.LENGTH_LONG).show()
+        val intentLogin = Intent(this, LoginActivity::class.java)
+        startActivity(intentLogin)
+    }
+
+    fun irACrearLugar(v: View){
+        //Hay diferentes maneras de imprimir mensajes en pantalla, unos más elaborados que otros.
+        val intentCrearLugar = Intent(this, CrearLugarActivity::class.java)
+        startActivity(intentCrearLugar)
+
     }
 }
