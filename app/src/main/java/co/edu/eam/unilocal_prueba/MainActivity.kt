50f70e5d56b95288484f742ee584a7bdb92458f1
DanyVillalbaT <villalba.daniela.0874@eam.edu.co>
2024-02-28 20:06:58 -0500

    Creados métodos para guardar y recuperar información

--- app/src/main/java/co/edu/eam/unilocal_prueba/MainActivity.kt
+++ app/src/main/java/co/edu/eam/unilocal_prueba/MainActivity.kt
@@ -5,13 +5,39 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var texto: TextView
+    lateinit var textoInicial: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        texto = findViewById(R.id.texto_inicial)
+
+        if (savedInstanceState != null){
+            textoInicial = savedInstanceState.getString("texto").toString()
+            texto.text = textoInicial
+        }else{
+            textoInicial = "Texto Inicial"
+            texto.text = textoInicial
+        }
+
         Log.e("MainActivity", "onCreate()")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("texto", textoInicial)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Log.e("MainActivity", savedInstanceState.getString("texto").toString())
     }
 
     override fun onStart() {
@@ -58,5 +84,10 @@ class MainActivity : AppCompatActivity() {
         val intentCrearLugar = Intent(this, CrearLugarActivity::class.java)
         startActivity(intentCrearLugar)
 
+    }
+
+    fun cambiarTexto(v: View){
+        textoInicial = "Nuevo valor"
+        texto.text = textoInicial
     }
 }
